b8201e9cfe3362b8048ca3c902e3b12922c6252e
parsaporogrammer <parsafarjami138701@gmail.com>
2023-09-10 19:05:01 +0330

    zist_haftom_hashtom

---
+++ zist sait/script.js
@@ -0,0 +1,47 @@
+const menuContainer1 = document.querySelector('.ha');
+
+menuContainer1.addEventListener('click' , function(){
+    document.querySelector('.a').classList.toggle('active');
+})
+
+const menuContainer2 = document.querySelector('.hb');
+
+menuContainer2.addEventListener('click' , function(){
+    document.querySelector('.b').classList.toggle('active');
+})
+
+const menuContainer3 = document.querySelector('.hc');
+
+menuContainer3.addEventListener('click' , function(){
+    document.querySelector('.c').classList.toggle('active');
+})
+
+const menuContainer4 = document.querySelector('.hd');
+
+menuContainer4.addEventListener('click' , function(){
+    document.querySelector('.d').classList.toggle('active');
+})
+
+const menuContainer5 = document.querySelector('.he');
+
+menuContainer5.addEventListener('click' , function(){
+    document.querySelector('.e').classList.toggle('active');
+})
+
+const menuContainer6 = document.querySelector('.hf');
+
+menuContainer6.addEventListener('click' , function(){
+    document.querySelector('.f').classList.toggle('active');
+})
+
+const menuContainer7 = document.querySelector('.hg');
+
+menuContainer7.addEventListener('click' , function(){
+    document.querySelector('.g').classList.toggle('active');
+})
+
+const menuContainer8 = document.querySelector('.hw');
+
+menuContainer8.addEventListener('click' , function(){
+    document.querySelector('.w').classList.toggle('active');
+})
